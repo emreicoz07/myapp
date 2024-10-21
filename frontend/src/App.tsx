ef402fd981a1bdb1da0bb702efb37d5f0a8460f1
emreicoz07 <ramazanemreicoz@gmail.com>
2024-10-21 17:15:28 +0200

    services page added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // React Router bileşenlerini import ediyorum
 import Home from './components/Home';
 import Navbar from './components/Navbar';
+import Services from './components/Services';
 
 const App: React.FC = () => (
   <Router>
@@ -11,8 +12,9 @@ const App: React.FC = () => (
     <div className="App">
       <Navbar /> {/* Navbar burada yer alacak */}
       <Routes>
-        <Route path="/" element={<Home />} /> {/* Home sayfası */}
-        {/* Diğer sayfaları buraya ekleyebilirsin */}
+        <Route path="/" element={<Home />} /> {/* Home Route */}
+        <Route path="/services" element={<Services />} /> {/* Services Route */}
+        {/* Diğer sayfaları buraya ekleyebilirsin - You can Add another Pages here */}
       </Routes>
     </div>
   </Router>
