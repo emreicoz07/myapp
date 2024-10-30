b5bcfaeaf0b9274d0fb683fc4eb6c56dad77e2a1
emreicoz07 <ramazanemreicoz@gmail.com>
2024-10-30 15:40:52 +0100

    app.tsx changed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,22 +3,28 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Re
 import Home from './components/Home';
 import Navbar from './components/Navbar';
 import Services from './components/Services';
+import About from 'components/About';
 import Appointment from 'components/Appointment';
 import Contact from 'components/Contact';
 import Footer from 'components/Footer';
->>>>>>> backup
 
 const App: React.FC = () => (
   <Router>
     {/* Burada Router ile sarılı olmalı */}
     <div id="root">
       <Navbar /> {/* Navbar burada yer alacak */}
-      <Routes>
-        <Route path="/" element={<Home />} /> {/* Home Route */}
-        <Route path="/services" element={<Services />} /> {/* Services Route */}
-        <Route path="/appointment" element={<Appointment />} />
-        {/* Appointment Route */}
-      </Routes>
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} /> {/* Home Route */}
+          <Route path="/services" element={<Services />} />{' '}
+          {/* Services Route */}
+          <Route path="/appointment" element={<Appointment />} />
+          {/* Appointment Route */}
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
+      <Footer />
     </div>
   </Router>
 );
